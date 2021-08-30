cd2575e4e52a407a475d83091e49eac8d7abf3e4
atmaniali <atmaniali97@gmail.com>
2021-08-30 12:07:40 +0100

    some modification in app & init

--- src/mesurments/apps.py
+++ src/mesurments/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class MesurmentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mesurments'
+    verbose_name = ' Mesurments between 2 locations '
---
+++ src/mesurments/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+app_name = 'mesurments'
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+]
